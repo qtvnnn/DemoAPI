a7ee35cdb7663487d740e94943cdb02fc455bd4a
NANG NGUYEN NGOC <nnnang@rd.local>
2021-05-06 09:00:52 +0700

    up

---
+++ MISA.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.Core.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        /// <summary>
+        /// Lấy toàn bộ dữ liệu trong db
+        /// </summary>
+        /// <returns>Danh sách đối tượng</returns>
+        /// CreatedBy: NNNANG (06/05/21)
+        IReadOnlyList<T> Get();
+        /// <summary>
+        /// Lấy thông tin một đối tượng theo id
+        /// </summary>
+        /// <param name="entityId">Id của đối tượng</param>
+        /// <returns>đối tượng có id tương ứng</returns>
+        T GetById(Guid entityId);
+        int Insert(T entity);
+        int Update(T entity, Guid entityId);
+    }
+}
